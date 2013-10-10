a45431942fb5fb6bad4c98490f7d6e98f0f98f91
Susumu SUZUNASHI <kaioh11922960@yahoo.co.jp>
2013-10-10 22:33:05 +0900

    Update pixivreply.js
    送信ボタンを押したらボタンを無効化（二度押し防止）

--- pixivreply.js
+++ pixivreply.js
@@ -3,9 +3,11 @@ $(".works_display").after('<div id="replybox" style="margin:10px;"></div>');
 var target = $("#replybox");
 target.append('<p style="color:#FF0000">※pixivReplyの利用により生じたいかなる損失・損害においても、鈴無蒸(pixivID:278010)は一切の責任を負わないものとします。</p>');
 $($(".comment-item").get().reverse()).each(function(i) {
-	target.append('<div class="pr_box" style="margin:10px;">' + $(this).text() +'<br /><textarea id="pr_txt' + i + '" style="width:600px;" maxlength="140">' + $("a",this).text() + 'さん&gt;</textarea><input id="pr_btn' + i + '" type="button" value="返信" /><font id="pr_status' + i + '" style="color:#808080">未送信</font></div>');
+	target.append('<div class="pr_box" style="margin:10px;">' + $(this).text() +'<br /><textarea id="pr_txt' + i + '" style="width:600px;" maxlength="140">＞' + $("a",this).text() + 'さん：</textarea><input id="pr_btn' + i + '" type="button" value="返信" /><font id="pr_status' + i + '" style="color:#808080">未送信</font></div>');
 	$("textarea", this).maxlength();
 	$(document).on('click', '#pr_btn' + i, function(){
+			$('#pr_btn' + i).val("送信中");
+			$('#pr_btn' + i).attr("disabled", true);
 			$.ajax({
 				type: 'post',
 				url: 'http://www.pixiv.net/member_illust.php',
@@ -22,12 +24,14 @@ $($(".comment-item").get().reverse()).each(function(i) {
 				success: function(data, textStatus){
 					$('#pr_status' + i).text("送信成功(多分)");
 					$('#pr_status' + i).css("color","#008000");
-					$('#pr_txt' + i).attr("disabled","disabled");
+					$('#pr_txt' + i).attr("disabled", true);
 					$('#pr_txt' + i).css("background-color","#A9A9A9");
+					$('#pr_btn' + i).hide('normal');
 				},
 				error: function(data, textStatus){
 		　　		$('#pr_status' + i).text("送信失敗");
 					$('#pr_status' + i).css("color","#FF0000");
+					$('#pr_btn' + i).attr("disabled",false);
 		　		}
 			});
 	});
